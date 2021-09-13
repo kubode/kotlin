11d99634415f32e3d8c64ab043c3ce73b976b264
Jinseong Jeon <jsjeon@google.com>
2021-09-13 11:00:41 -0700

    FIR IDE: type-aware constant conversion

---
+++ idea/idea-frontend-fir/tests/org/jetbrains/kotlin/idea/fir/frontend/api/components/AbstractCompileTimeConstantEvaluatorTest.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.fir.frontend.api.components
+
+import org.jetbrains.kotlin.idea.fir.executeOnPooledThreadInReadAction
+import org.jetbrains.kotlin.idea.fir.frontend.api.test.framework.AbstractHLApiSingleFileTest
+import org.jetbrains.kotlin.idea.fir.low.level.api.test.base.expressionMarkerProvider
+import org.jetbrains.kotlin.idea.frontend.api.analyse
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.test.model.TestModule
+import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.assertions
+
+abstract class AbstractCompileTimeConstantEvaluatorTest : AbstractHLApiSingleFileTest() {
+    override fun doTestByFileStructure(ktFile: KtFile, module: TestModule, testServices: TestServices) {
+        val expression = testServices.expressionMarkerProvider.getSelectedElement(ktFile) as KtExpression
+        val constantValue = executeOnPooledThreadInReadAction {
+            analyse(expression) { expression.evaluate() }
+        }
+        val actual = buildString {
+            appendLine("expression: ${expression.text}")
+            appendLine("constant_value: $constantValue")
+            appendLine("constant: ${constantValue?.toConst()}")
+        }
+        testServices.assertions.assertEqualsToFile(testDataFileSibling(".txt"), actual)
+    }
+}
